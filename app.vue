8e580ae9b5ff70d23b1520e4027d74884ed88daf
Shubham Pawar <shubhampawar3007@gmail.com>
2025-05-31 19:32:43 +0530

    chore: tailwindcss and icon tabular

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <UButton icon="tabler:map">
+      Hello world
+    </UButton>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
@@ -9,7 +11,14 @@ export default defineNuxtConfig({
     "@nuxt/icon",
     "@nuxt/image",
     "@nuxt/ui",
+    "@nuxtjs/color-mode",
   ],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       standalone: false, // <---
